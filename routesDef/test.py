2e5d024e0a45d94e80baf44bbd3a0fdb29438add
bny11638 <yaudev2601@gmail.com>
2021-01-12 14:37:42 -0500

    test endpoint

---
+++ routesDef/test.py
@@ -0,0 +1,11 @@
+from flask import escape
+def hello_http(request):
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(escape(name))
