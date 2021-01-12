f7e21d9b770e7994b845b480563722e2ac10792a
bny11638 <yaudev2601@gmail.com>
2021-01-12 16:24:51 -0500

    Hopefully it works

--- app/routesDef/main.py
+++ app/routesDef/main.py
@@ -1,18 +1,52 @@
 from flask import escape
-import localPackage.mysql.connector
+import sqlalchemy
+# Uncomment and set the following variables depending on your specific instance and database:
+connection_name = "precise-truck-301217:us-central1:nutrition-uga"
+#table_name = ""
+#table_field = ""
+#table_field_value = ""
+db_name = "nutrition_app"
+db_user = "guest"
+db_password = "password"
 
-conn = localPackage.mysql.connector.connect (
-        host="35.224.143.155",
-        user="guest",
-        password="password",
-        database="nutrition_app",
-)
+# If your database is MySQL, uncomment the following two lines:
+driver_name = 'mysql+pymysql'
+query_string = dict({"unix_socket": "/cloudsql/{}".format(connection_name)})
+
+# If your database is PostgreSQL, uncomment the following two lines:
+#driver_name = 'postgres+pg8000'
+#query_string =  dict({"unix_sock": "/cloudsql/{}/.s.PGSQL.5432".format(connection_name)})
+
+# If the type of your table_field value is a string, surround it with double quotes.
 
 def insert(request):
+
     request_json = request.get_json()
     if request_json and 'name' and 'password' in request_json:
         name = request_json['name']
         password = request_json['password']
+        table_name = "user_data"
+        table_field="username,password"
+        table_field_value = name + "," + password
+        stmt = sqlalchemy.text('INSERT INTO table_name (table_field) VALUES (table_field_value)')
+    db = sqlalchemy.create_engine(
+      sqlalchemy.engine.url.URL(
+        drivername=driver_name,
+        username=db_user,
+        password=db_password,
+        database=db_name,
+        query=query_string,
+      ),
+      pool_size=5,
+      max_overflow=2,
+      pool_timeout=30,
+      pool_recycle=1800
+    )
+    try:
+        with db.connect() as conn:
+            conn.execute(stmt)
+    except Exception as e:
+        return 'Error: {}'.format(str(e))
     return 'Hello {}! Your Password is {}'.format(escape(name,password))
 
 def hello_http(request):
@@ -24,7 +58,7 @@ def hello_http(request):
         name = request_args['name']
     else:
         name = 'World'
-    return 'Hello {}!'.format(escape(name))
+    return 'It worked ! Hello {}!'.format(escape(name))
 
 def show_user(request):
     request_json = request.get_json(silent=True)
