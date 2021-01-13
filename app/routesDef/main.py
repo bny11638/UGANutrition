0b6c0edbd24c9eb8fb7fe872d6e9a495e7da1877
bny11638 <yaudev2601@gmail.com>
2021-01-12 20:44:50 -0500

    Editing

--- app/routesDef/main.py
+++ app/routesDef/main.py
@@ -20,53 +20,6 @@ query_string = dict({"unix_socket": "/cloudsql/{}".format(connection_name)})
 
 # If the type of your table_field value is a string, surround it with double quotes.
 
-def insert(request):
-    request_json = request.get_json()
-    request_args = request.args
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
-    else:
-        name = 'World'
-    return 'It worked ! Hello {}!'.format(escape(name))
-    """
-    print(str(request))
-    try:
-        request_json = request.get_json(force=True)
-        request_args = request.args
-    except:
-        return(request)
-    if request_json and 'name' and 'password' in request_json:
-        nameJ = request_json['name']
-        passwordJ = request_json['password']
-        table_name = "user_data"
-        table_field="username,password"
-        table_field_value = nameJ + "," + passwordJ
-        #stmt = (insert(table_name).values(username=nameJ,password=passwordJ))
-    """
-    """
-    db = sqlalchemy.create_engine(
-      sqlalchemy.engine.url.URL(
-        drivername=driver_name,
-        username=db_user,
-        password=db_password,
-        database=db_name,
-        query=query_string,
-      ),
-      pool_size=5,
-      max_overflow=2,
-      pool_timeout=30,
-      pool_recycle=1800
-    )
-    try:
-        with db.connect() as conn:
-            conn.execute(stmt)
-    except Exception as e:
-        return 'Error: {}'.format(str(e))
-    """
-    #return 'Hello {}! Your Password is {}'.format(escape(name,password))
-
 def hello_http(request):
     request_json = request.get_json()
     request_args = request.args
@@ -81,10 +34,11 @@ def hello_http(request):
 def insert_test(request):
     request_json = request.get_json()
     request_args = request.args
-    if request_json and 'name' in request_json:
+    if request_json and 'name' and 'password' in request_json:
         name = request_json['name']
+        password = request_json['password']
     elif request_args and 'name' in request_args:
         name = request_args['name']
     else:
         name = 'World'
-    return 'It worked ! Hello{}!'.format(escape(name))
+    return 'It worked ! Hello{} your password is {}!'.format(escape(name,password))
