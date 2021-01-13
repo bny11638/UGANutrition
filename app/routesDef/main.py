e28e7e2aafdedcac518d0483781916853a3507c5
bny11638 <yaudev2601@gmail.com>
2021-01-13 11:33:52 -0500

    check_register

--- app/routesDef/main.py
+++ app/routesDef/main.py
@@ -75,8 +75,7 @@ def check_Register(request):
                 stmt = sqlalchemy.text("SELECT * FROM user_data where username = \'" + name+"\';")
                 with db.connect() as conn:
                     query = conn.execute(stmt)
-                    print(query)
-                return True
+                return query
         except Exception as e:
             return 'Error with get: {}'.format(str(e))
     return False
