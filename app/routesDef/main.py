7e4986f1abd81175140ba182b7a5a2ae2d854239
bny11638 <yaudev2601@gmail.com>
2021-01-12 20:13:02 -0500

    help

--- app/routesDef/main.py
+++ app/routesDef/main.py
@@ -21,8 +21,11 @@ query_string = dict({"unix_socket": "/cloudsql/{}".format(connection_name)})
 # If the type of your table_field value is a string, surround it with double quotes.
 
 def insert(request):
-    request_json = request.get_json(force=True)
-    request_args = request.args
+    try:
+        request_json = request.get_json(force=True)
+        request_args = request.args
+    except:
+        return(request)
     if request_json and 'name' and 'password' in request_json:
         nameJ = request_json['name']
         passwordJ = request_json['password']
@@ -60,14 +63,3 @@ def hello_http(request):
     else:
         name = 'World'
     return 'It worked ! Hello {}!'.format(escape(name))
-
-def show_user(request):
-    request_json = request.get_json()
-    request_args = request.args
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
-    else:
-        name = 'World'
-    return 'Hello {}!'.format(escape(name))
