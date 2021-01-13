c89e9dfcd56cb03efa77993f36264488b0b1f90d
bny11638 <yaudev2601@gmail.com>
2021-01-12 21:08:41 -0500

    ugh

--- app/routesDef/main.py
+++ app/routesDef/main.py
@@ -37,6 +37,27 @@ def insert_test(request):
     if request_json and 'name' and 'password' in request_json:
         name = request_json['name']
         password = request_json['password']
+        stmt = sqlalchemy.text("INSERT INTO user_data (username, password) values (\'" + name + "\'," + "\'password\');")
+        db = sqlalchemy.create_engine(
+        sqlalchemy.engine.url.URL(
+            drivername=driver_name,
+            username=db_user,
+            password=db_password,
+            database=db_name,
+            query=query_string,
+        ),
+        pool_size=5,
+        max_overflow=2,
+        pool_timeout=30,
+        pool_recycle=1800
+        )
+    try:
+        with db.connect() as conn:
+            conn.execute(stmt)
+    except Exception as e:
+        return 'Error: {}'.format(str(e))
+    return 'ok'
+
     elif request_args and 'name' in request_args:
         name = request_args['name']
     else:
