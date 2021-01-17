b02548746ecd8f2f53f68faefaf61e0f3e827995
bny11638 <yaudev2601@gmail.com>
2021-01-17 10:26:35 -0500

    Testing new Setup

--- app/__main__.py
+++ app/__main__.py
@@ -68,24 +68,24 @@ class NutritionApp(Tk):
         self.connection.close()
     #initializes all images
     def initImage(self):
-        imguh = Image.open(find_data_file("login.png"))
+        imguh = Image.open("resources/login.png")
         imguh = imguh.resize((250, 60), Image.ANTIALIAS) ## The (250, 250) is (height, width
         im_login = ImageTk.PhotoImage(imguh)
         self.loginButtonImg = im_login
-        self.logo = ImageTk.PhotoImage(file=find_data_file("logosmall.png"))
-        register = Image.open(find_data_file("register.png"))
+        self.logo = ImageTk.PhotoImage(file="resources/logosmall.png")
+        register = Image.open("resources/register.png")
         register = register.resize((250, 60), Image.ANTIALIAS)
         im_register = ImageTk.PhotoImage(register)
         self.registerButtonImg = im_register
-        use_guest = Image.open(find_data_file("guest.png"))
+        use_guest = Image.open("resources/guest.png")
         use_guest = use_guest.resize((250, 60), Image.ANTIALIAS)
         im_guest = ImageTk.PhotoImage(use_guest)
         self.guestButtonImg = im_guest
-        back = Image.open(find_data_file("back.png"))
+        back = Image.open("resources/back.png")
         back = back.resize((80, 25), Image.ANTIALIAS)
         im_back = ImageTk.PhotoImage(back)
         self.backButtomImg = im_back
-        submit = Image.open(find_data_file("submit.png"))
+        submit = Image.open("resources/submit.png")
         submit = submit.resize((80, 25), Image.ANTIALIAS)
         im_submit = ImageTk.PhotoImage(submit)
         self.submitButtomImg = im_submit
